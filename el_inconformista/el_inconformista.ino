35a8cf60d061648612bab1c4e595cb6070ae11e2
leandroestrella <alexisleandro@gmail.com>
2020-08-09 20:09:39 +0200

    improved code and added randomness.

--- el_inconformista/el_inconformista.ino
+++ el_inconformista/el_inconformista.ino
@@ -1,159 +1,162 @@
-/**************************************************************************************
- * "Ma non è tanto questo, è il medium di massa in sé. Dal momento in cui qualcuno ci *
- *  ascolta dal video, ha verso di noi un rapporto da inferiore a superiore che è un  *
- *  rapporto spaventosamente antidemocratico."                                        *
- *                                                                Pier Paolo Pasolini *
- * "El Inconformista || The Nonconformist "                                           *
- *  by Leandro Estrella [https://cargocollective.com/leandroestrella]                 *
- **************************************************************************************/
+/**************************************************************************************************************
+ * "Ma non è tanto questo, è il medium di massa in sé. Dal momento in cui qualcuno ci ascolta dal video,      *
+ *  ha verso di noi un rapporto da inferiore a superiore che è un rapporto spaventosamente antidemocratico."  *
+ *                                                                                      - Pier Paolo Pasolini *
+ * "El Inconformista || The Nonconformist "                                                                   *
+ *  by Leandro Estrella [https://cargocollective.com/leandroestrella]                                         *
+ *************************************************************************************************************/
  
-/* Libraries */
-#include <VarSpeedServo.h>  // include VarSpeedServo library by Philip van Allen
-#include <Wire.h>           // include Wire library by Nicholas Zambetti and James Tichenor
-#include <SRF02.h>          // include SRF02 library by Dirk Grappendorf
-#include <TEA5767.h>        // include TEA5767 FM radio Module library by Vasilakis Michalis
+/* LIBRARIES */
+/* Sensor (SRF02) */
+/* Wire by Nicholas Zambetti and James Tichenor [https://www.arduino.cc/en/Reference/Wire] */
+#include <Wire.h>
+/* SRF02 by Dirk Grappendorf
+    [https://www.grappendorf.net/arduino-atmel-avr/arduino-libraries/srf02-ultrasonic-distance-sensor.html] */
+#include <SRF02.h>
 
-/* Constants (won't change) */
-SRF02 sensor (0x70, SRF02_CENTIMETERS);
-TEA5767 Radio;              // Pinout SLC and SDA - Arduino uno pins A5 and A4
-const int servoPin = 3;     // the pin number for the servo signal
-#define MS 105              // movement speed
-#define NN 18               // number of notes
-/* notes */
-#define X 35                // contact point angle for servo motor
-#define A 50
-#define B 60
-#define C 90
-#define D 120
+/* Radio (TEA5767) */
+/* TEA5767 FM Radio Module by big12boy [https://github.com/big12boy/TEA5767] */
+#include <TEA5767.h>
 
-/* Variables (will change) */
-/* Servo */
-VarSpeedServo myservo;            // create servo object to control a servo
-int servoPosition = 50;           // the current angle of the servo.
+/* Servo (HS-53) */
+/* VarSpeedServo by Philip van Allen [https://github.com/netlabtoolkit/VarSpeedServo] */
+#include <VarSpeedServo.h>
 
-/* SRF02 */
-unsigned long currentMillis = 0;  // stores the value of millis() in each iteration of loop()
-unsigned long nextPrintSRF = 0;   // ...
-int sensorValueSRF = 0;           // variable to store the value coming from the sensor
-int sensorValueSRFPrev = 0;       // variable to store the previous value of the sensor
+/* CONSTANTS */
+/* Sensor (SRF02) */
+SRF02 sensor (0x70, SRF02_CENTIMETERS); /* SRF02 instance with the specified (7-bit) device id and measurement
+                                           mode (SRF02_INCHES, SRF02_CENTIMETERS, SRF02_MICROSECONDS) */
+#define triggerValue 225                /* sensor value to trigger servo */
+#define srfWait 1000                    /* millis to wait to allow SRF02 print */
 
-/* TEA5767 */
-double old_frequency;
-double frequency;
-int search_mode = 0;
-int search_direction;
-unsigned long last_pressed;
-unsigned long nextPrintFM = 0;    // ...
+/* Radio (TEA5767) */
+TEA5767 radio = TEA5767();  /* pinout SLC and SDA - Arduino uno pins A5 and A4 */
+#define radioMinLvl 10      /* radio minimum required signal level (0-5=unuseable, 6-9=poor, 10-12=good,
+                                 13-14=really good, 15=excellent)  */
+#define radioWait 4000      /* millis to wait before changing station (delay) */
+
+/* Servo (HS-53) */
+#define servoPin 3          /* servo motor i/o pin */
+VarSpeedServo servo;        /* create servo object */
+#define servoSpeed 105      /* movement speed */
+#define servoContact 35     /* contact point with saucepan */
+#define servoIdle 50        /* idle position */
+#define servoWait 75        /* millis to wait to allow servoEnable (delay) */
+
+/* Debug */
+bool printEnabled = true;   /* sets serial debug (enabled(disabled) */
+
+/* VARIABLES */
+/* Sensor (SRF02) */
+unsigned long nextSRFPrint = 0;   /* stores next time to print sensor read (in millis) */
+int lastSensorValue= 0;           /* stores last sensor read value */
+int sensorValue = 0;              /* stores the value coming from the sensor */
+
+/* Radio (TEA5767) */
+unsigned long nextRadioPrint = 0; /* stores next time to print radio station (in millis) */
+
+/* Servo (HS-53) */
+bool servoEnabled = false;        /* stores servo state (enabled/disabled) */
+int servoPositions[4];            /* stores servo positions (notes) */
+int servoRepeat;                  /* times to repeat servo loop */
+unsigned long servoDelay = 0;     /* stores next time to enable servo (in millis) */
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Starting FM Radio, Servo and Sensor");
-  Wire.begin();
-  Radio.init();
-  Radio.set_frequency(106.00);      // on power on go to station
-  myservo.write(servoPosition);     // sets the initial position
+  randomSeed(analogRead(0));      /* if analog input pin 0 is unconnected, random analog noise will cause
+                                     the call to randomSeed() to generate different seed numbers each time
+                                     the sketch runs. randomSeed() will then shuffle the random function. */
+  Wire.begin();                   /* join i2c bus (address optional for master) */
+  SRF02::setInterval(70);         /* set the measurement intervall in milliseconds */
+  radio.init(radioMinLvl);        /* initialize needed features and search for stations */
+  servo.attach(servoPin);                   /* attach servo to servoPin */
+  servo.write(servoIdle, servoSpeed, true); /* sets the initial position */
+  servo.detach();                           /* stop attached servo from pulsing its i/o pin */
+
+  /* Enable/Disable Printing */
+  if (printEnabled) {
+    Serial.begin(9600);                 /* start serial communication at 9600bps */
+    Serial.println("Starting Radio, Servo and Sensor");
+    Serial.print("Found ");
+    Serial.print(radio.getStations());  /* get number of stations from library */
+    Serial.println(" station(s)");
+  }
 }
 
 void loop() {
-  // Notice that none of the action happens in loop() apart from reading
-  // millis() it just calls the functions that have the action code
-  currentMillis = millis();
-  sensorSense();              //  starts srf02 sensor loop
-  servoSong();                //  starts servo protest song loop
-  fmRadio();                  //  starts tea5767 fm radio loop
-}
-
-void sensorSense() {
-  SRF02::update();
-  if (millis() > nextPrintSRF) {
-    sensorValueSRF = sensor.read();
-    if (sensorValueSRF != sensorValueSRFPrev) {
-    Serial.print("SRF02 ");
-    Serial.print(sensorValueSRF);
-    Serial.println("cm");
-    sensorValueSRFPrev = sensorValueSRF;
-    nextPrintSRF = millis () + 75;
+  if (servoEnabled) {
+    /* assign random values to servoPositions array */
+    for (byte i = 0; i < (sizeof(servoPositions) / sizeof(servoPositions[0])); i++) {
+      servoPositions[i] = random(servoIdle, 100);
     }
-  }
-}
-
-void servoSong() {
-  int sensorValue = sensor.read();
-  if (millis() > nextPrintSRF) {
-    if ((sensorValue > 0) && (sensorValue < 150)) {
-      myservo.attach(servoPin);
-      myservo.write(C, MS, true);     // go to position C at speed of MS and wait till finish
-      myservo.write(X, MS, true);
-      myservo.write(B, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(B, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(B, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(D, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(D, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(D, MS, true);
-      myservo.write(C, MS, true);     // go to position C at speed of MS and wait till finish
-      myservo.write(X, MS, true);
-      myservo.write(B, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(B, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(B, MS, true);
-      myservo.write(X, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(D, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(D, MS, true);
-      myservo.write(C, MS, true);
-      myservo.write(D, MS, true);
-    } else {
-      myservo.detach();
+    servo.attach(servoPin); /* attach servo to servoPin */
+    servoRepeat = random(4, 8);
+    
+    /* print servo positions and repeat times */
+    if (printEnabled) {
+      Serial.print("Servo Positions: ");
+      for (byte i = 0; i < (sizeof(servoPositions) / sizeof(servoPositions[0])); i++) {
+        Serial.print(servoPositions[i]);
+        Serial.print(", ");
+      }
+      Serial.print("Repeat Times: ");
+      Serial.println(servoRepeat);
     }
-  }
-}
 
-void fmRadio() {
-  int sensorValueFM = sensor.read();
-  unsigned char buf[5];
-  int stereo;
-  int signal_level;
-  double current_freq;
-
-  if (millis() > nextPrintFM) {
-    if (Radio.read_status(buf) == 1) {
-      current_freq =  floor (Radio.frequency_available (buf) / 100000 + .5) / 10;
-      stereo = Radio.stereo(buf);
-      signal_level = Radio.signal_level(buf);
-      Serial.print("FM: ");
-      Serial.print(current_freq);
-      Serial.print(" Signal level: ");
-      Serial.print(signal_level);
-      if (stereo) {
-        Serial.println(" STEREO ");
-      } else {
-        Serial.println(" MONO ");
+    /* play servo notes */
+    for (int i = 0 ; i < servoRepeat ; i++) {
+      for (byte i = 0; i < (sizeof(servoPositions) / sizeof(servoPositions[0])); i++) {
+        /* go to servoPosition/servoContact at speed of servoSpeed and wait till finish */
+        servo.write(servoPositions[i], servoSpeed, true);
+        servo.write(servoContact, servoSpeed, true);
       }
     }
-    if (search_mode == 1) {
-      if (Radio.process_search (buf, search_direction) == 1) {
-        search_mode = 0;
+    servo.write(servoIdle, servoSpeed, true); /* sets the initial position */
+    servo.detach();                           /* stop attached servo from pulsing its i/o pin */
+    servoEnabled = false;
+    servoDelay = millis() + servoWait;
+    
+    /* print servo state */
+    if (printEnabled) {
+      Serial.print("Servo Disabled, waiting for: ");
+      Serial.println(servoDelay);
+    }
+    
+  } else if (!servoEnabled) {
+    SRF02::update();              /* update sensor data */
+    sensorValue = sensor.read();  /* read the current distance value */
+    
+    /* print sensor read */
+    if (printEnabled) {
+      /* check if its different that last read */
+      if ((millis() > nextSRFPrint) && (sensorValue != lastSensorValue) && (sensorValue >= 0)) {
+        Serial.print("SRF02 ");
+        Serial.print(sensorValue);
+        Serial.println("cm");
+        lastSensorValue = sensorValue;
+        nextSRFPrint = millis() + srfWait;
       }
     }
-    search_mode = 1;
-    search_direction = TEA5767_SEARCH_DIR_UP;
-    Radio.search_up(buf);
-    nextPrintFM = millis () + 2500;
+    if (millis() > servoDelay) {
+      if ((sensorValue > 0) && (sensorValue < triggerValue)) {
+        servoEnabled = true;
+        
+        /* print servo status */
+        if (printEnabled) {
+          Serial.println("Servo Enabled");
+        }
+      }
+    }
+    if (millis() > nextRadioPrint) {
+      radio.nextStation();  /* go to next station */
+      nextRadioPrint = millis() + radioWait;
+      
+      /* print radio status */
+      if (printEnabled) {
+        Serial.print("Radio Frequency: ");
+        Serial.print(radio.getFrequency());       /* current frequency */
+        Serial.print(" Signal Level: ");
+        Serial.println(radio.getSignalLevel());   /* current signal level (0-15) */
+      }
+    }
   }
 }
